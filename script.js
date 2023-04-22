98d6c429838045d9d77e3c2abe2971fe05014b33
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-04-22 14:28:52 +0530

    Stop watch updated

--- script.js
+++ script.js
@@ -1,61 +1,41 @@
-var timeBegan = null;
-var timeStopped = null;
-var stoppedDuration = 0;
-var startInterval = null;
-var flag = false;
 
-document.querySelector(".container").addEventListener("click", () => {
-    if (!flag) {
-        startTimer();
-        flag = true;
-    }
-    else {
-        stopTimer();
-        flag = false;
-    }
-})
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const milliseconds = document.querySelector('.milliseconds');
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
 
-document.querySelector(".container").addEventListener("dblclick", () => {
-    resetTimer();
-})
+let startTime;
+let interval;
 
-startTimer = () => {
-    if (timeBegan === null)
-        timeBegan = new Date();
-
-    if (timeStopped !== null)
-    stoppedDuration += (new Date() - timeStopped);
-
-    startInterval = setInterval(clockRunning, 10);
+function start() {
+  startTime = Date.now();
+  interval = setInterval(updateDisplay, 10);
 }
 
-stopTimer = () => {
-    timeStopped = new Date();
-    clearInterval(startInterval);
+function stop() {
+  clearInterval(interval);
 }
 
-clockRunning = () => {
-    let currentTime = new Date();
-    let timeElapsed = new Date(currentTime - timeBegan - stoppedDuration);
-
-    let minutes = timeElapsed.getUTCMinutes();
-    let seconds = timeElapsed.getUTCSeconds();
-    let milliseconds = timeElapsed.getUTCMilliseconds();
-
-    milliseconds = Math.floor(milliseconds / 10);
-
-    document.querySelector('#timer_display').textContent = 
-    (minutes = minutes < 10 ? '0' + minutes : minutes) + ":" +
-    (seconds = seconds < 10 ? '0' + seconds : seconds) + ":" +
-    (milliseconds = milliseconds < 10 ? '0' + milliseconds : milliseconds);
+function reset() {
+  clearInterval(interval);
+  minutes.textContent = '00';
+  seconds.textContent = '00';
+  milliseconds.textContent = '00';
 }
 
+function updateDisplay() {
+  const elapsedTime = Date.now() - startTime;
+  const minutesValue = Math.floor(elapsedTime / 60000);
+  const secondsValue = Math.floor((elapsedTime - (minutesValue * 60000)) / 1000);
+  const millisecondsValue = Math.floor((elapsedTime - (minutesValue * 60000) - (secondsValue * 1000)) / 10);
 
-resetTimer = () => {
-    clearInterval(startInterval);
-    timeBegan = null;
-    timeStopped = null;
-    stoppedDuration = 0;
-    document.querySelector('#timer_display').innerHTML = "00:00:00";
-    flag = false;
+  minutes.textContent = minutesValue < 10 ? '0' + minutesValue : minutesValue;
+  seconds.textContent = secondsValue < 10 ? '0' + secondsValue : secondsValue;
+  milliseconds.textContent = millisecondsValue < 10 ? '0' + millisecondsValue : millisecondsValue;
 }
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);
+resetBtn.addEventListener('click', reset);
